72d98d1bf8028eb951783e84eeee1167334438bc
odromero1717@gmail.com <IbrIbr2020*>
2020-11-14 10:55:03 -0500

    first commit

---
+++ src/main/java/oscar/group/MarketOscar/MarketOscarApplication.java
@@ -0,0 +1,13 @@
+package oscar.group.MarketOscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketOscarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketOscarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oscar/group/MarketOscar/MarketOscarApplicationTests.java
@@ -0,0 +1,13 @@
+package oscar.group.MarketOscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketOscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
