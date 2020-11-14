118c776ef2bdb68eb6b2ab1316fc7a7731253dc2
odromero1717@gmail.com <IbrIbr2020*>
2020-11-14 10:51:48 -0500

    first commit

---
+++ src/main/java/oscar/group/MarketOscar/HolaMundoControlador.java
@@ -0,0 +1,16 @@
+package oscar.group.MarketOscar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeralinea")
+
+public class HolaMundoControlador {
+     @GetMapping("/hope")
+    public String primeralinea()
+    {
+        return "hope  primera linea";
+    }
+}
